66e80616198001cca24c4a51bee516848bc974ec
sw1art <swioneart@gmail.com>
2023-05-21 00:09:26 +0300

    4

---
+++ 4/django_project/accounts/tests.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class CustomUserTest(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='will', 
+            email='will@email.com',
+            password='testpass123',
+            )
+        self.assertEqual(user.username, 'will')
+        self.assertEqual(user.email, 'will@email.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_superuser(
+            username='admin', 
+            email='admin@email.com',
+            password='testpass123',
+            )
+        self.assertEqual(user.username, 'admin')
+        self.assertEqual(user.email, 'admin@email.com')
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
